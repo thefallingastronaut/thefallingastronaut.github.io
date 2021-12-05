f39583b0bb84a2ea69faf4b09edac28029ed07b2
thefallingastronaut <thefallingastronaut@gmail.com>
2021-12-05 12:46:56 +0700

    first commit

---
+++ lk21.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('.menu-btn').click(function () {
+    // list.classList.add('show');
+    $('.nav-list2').slideToggle(200);
+  });
+});
+$(document).ready(function () {
+  $('.menu-btn3').click(function () {
+    // list.classList.add('show');
+    $('.nav3').slideToggle(200);
+  });
+});
